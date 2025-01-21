ebfd84eef02660b4acd38b6154c7f15bced933cc
JabedAhmedRifat <jabedahmedrifat1@gmail.com>
2025-01-21 11:49:37 +0600

    CORS

--- MovieRaterApi/movierater/settings.py
+++ MovieRaterApi/movierater/settings.py
@@ -39,18 +39,27 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+
+
 
 ROOT_URLCONF = 'movierater.urls'
 
